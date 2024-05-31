8f82c63b7fa981fefa285849ae876cab50eb62c7
Mahesh Shetty <mahesh.shetty@capillarytech.com>
2024-05-31 13:04:01 +0530

    Feat -> added a logo for app

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -25,7 +25,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.statusBarColor = Color.Transparent.toArgb()
         setContent {
             MyApplicationTheme {
                 Surface(
